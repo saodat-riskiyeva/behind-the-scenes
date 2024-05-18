43c320ababca322675bce245f2740a684239df02
Saodat <saodat.riskiyeva@gmail.com>
2024-05-18 12:33:51 -0400

    practising hoisting and TDZ

--- starter/script.js
+++ starter/script.js
@@ -25,3 +25,29 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+
+console.log(age);
+
+var age = 37;
+
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
