fb6e7aafb389da3b5dea04582e226f945ba8b8c2
Saodat <saodat.riskiyeva@gmail.com>
2024-05-18 12:00:41 -0400

    practising scoping

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var milleanial = true;
+      const str = `Oh, and you are a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(milleanial);
+  }
+
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
